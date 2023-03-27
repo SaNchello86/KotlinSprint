8cecab8a9a90e30c50ec46a40ef3df9aabb6b347
SaNchello <sahokkk@mail.ru>
2023-03-27 21:35:47 +0300

    KS-1-2 27.03.2023 change (lession_1_task_1.kt, lession_1_task_2.kt)

--- src/main/kotlin/lession_1/lession_1_task_1.kt
+++ src/main/kotlin/lession_1/lession_1_task_1.kt
@@ -1,6 +1,6 @@
 package lession_1
 fun main (){
-    val NumberOfOrder: Int = 75 // Колличество заказов
+    val numberOfOrder: Int = 75 // Колличество заказов
     val thanksOrder: String = ("Благарим за покупку в нашем магазине! По возможности оставьте отзыв о Нашем сорвисе.")
-    println (thanksOrder)
+    println ()
 }
--- src/main/kotlin/lession_1/lession_1_task_2.kt
+++ src/main/kotlin/lession_1/lession_1_task_2.kt
@@ -1,7 +1,7 @@
 package lession_1
 
 fun main (){
-    val NumberOfOrder: Int = 75 // Колличество заказов
+    val numberOfOrder: Int = 75 // Колличество заказов
     val thanksOrder: String = ("Благарим за покупку в нашем магазине! По возможности оставьте отзыв о Нашем сорвисе.")
     println (thanksOrder)
 
