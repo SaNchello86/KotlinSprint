a253a0817691440acd6f980b89b7fd05b3ddd7cc
SaNchello <sahokkk@mail.ru>
2023-03-27 16:48:45 +0300

    KS-1-2 27.03.2023 Initializing variables first lession_1_2

---
+++ src/main/kotlin/lession_1/lession_1_task_2.kt
@@ -0,0 +1,13 @@
+package lession_1
+
+fun main (){
+    val NumberOfOrder: Int = 75 // Колличество заказов
+    val thanksOrder: String = ("Благарим за покупку в нашем магазине! По возможности оставьте отзыв о Нашем сорвисе.")
+    println (thanksOrder)
+
+    var staff: Int = 2000 //Колличество работников
+    //println(staff)
+
+    staff = 1999
+    println(staff)
+}
