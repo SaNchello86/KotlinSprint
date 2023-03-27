71b9926ba443c611b75efe9cf9d389cf02b30886
SaNchello <sahokkk@mail.ru>
2023-03-27 21:20:40 +0300

    KS-1-2 27.03.2023 lession_1_3

---
+++ src/main/kotlin/lession_1/lession_1_task_3.kt
@@ -0,0 +1,17 @@
+package lession_1
+
+fun main () {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Год полёта - " + year)
+    println("Время взлёта - ")
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("Время посадки - " + hour +":"+ minute)
+}
