99f04ffa2cef666e0923996d93f709f7c367a616
SaNchello <sahokkk@mail.ru>
2023-03-28 22:29:26 +0300

    KS-1-3 28.03.2023 Исправление с учётом указанных пунктов.

--- src/main/kotlin/lession_1/Lession_1_task_3.kt
+++ src/main/kotlin/lession_1/Lession_1_task_3.kt
@@ -2,16 +2,17 @@ package lession_1
 
 fun main () {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println("Год полёта Юрия Гагарина - " + year)
     println("Время взлёта - ")
     println(hour)
+    println(":")
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("Время посадки - " + hour +":"+ minute)
 }
