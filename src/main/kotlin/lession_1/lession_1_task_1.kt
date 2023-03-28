06f5456ba1554693aa532c4ab0919f7935d712b1
SaNchello <sahokkk@mail.ru>
2023-03-28 22:16:54 +0300

    KS-1-4 28.03.2023 Исправление с учётом указанных пунктов.

--- src/main/kotlin/lession_1/lession_1_task_1.kt
+++ src/main/kotlin/lession_1/lession_1_task_1.kt
@@ -2,6 +2,5 @@ package lession_1
 fun main (){
     val numberOfOrder: Int = 75 // Колличество заказов
     val thanksOrder: String = ("Благарим за покупку в нашем магазине! По возможности оставьте отзыв о Нашем сорвисе.")
-    println ("Заказов - " + numberOfOrder)
-    println(thanksOrder)
+
 }
