52f6c90cc958fe1e94c35070e2b7b1dab8a9e5cb
SaNchello <sahokkk@mail.ru>
2023-04-06 21:22:01 +0300

    KS-1-5 28.03.2023 исправленны ошибки.

--- src/main/kotlin/lession_1/lession_1_task_5.kt
+++ src/main/kotlin/lession_1/lession_1_task_5.kt
@@ -1,9 +1,10 @@
 package lession_1
 
 fun main() {
-    var secondYG = 6480/60
-    val result = secondYG-60
 
-        println("Время проведённое в космосе Юрием Гагариным: - " + result + ":00")
+    val secmondsInSpace = 6480 // время полёта
+    val inSpaceMinute = 6480 / 60 - 60
+
+    println ("$inSpaceMinute:00")
 
 }
