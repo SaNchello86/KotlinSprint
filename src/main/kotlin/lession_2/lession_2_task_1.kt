9e9e5672d4c27771737bf7550aae3caf73f2838f
SaNchello <sahokkk@mail.ru>
2023-04-07 16:03:13 +0300

    KS-2-1 07.04.2023 Решение задачи на среднеарифметическое.

---
+++ src/main/kotlin/lession_2/lession_2_task_1.kt
@@ -0,0 +1,13 @@
+package lession_2
+
+fun main () {
+
+    val igor = 3
+    val pavel = 4
+    val sasha = 3
+    val petya = 5
+    val averageScore: Float = (3+4+3+5)/4f
+
+    println(averageScore)
+
+}
