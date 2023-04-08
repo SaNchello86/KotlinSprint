08efadd710574cb95d5e18adc08104b0a81a0611
SaNchello <sahokkk@mail.ru>
2023-04-08 17:19:41 +0300

    KS-2-2 07.04.2023 Зарплата сотрудников. исправленны недочёты.

--- src/main/kotlin/lession_2/lession_2_task_1.kt
+++ src/main/kotlin/lession_2/lession_2_task_1.kt
@@ -1,12 +1,12 @@
 package lession_2
 
-fun main () {
+fun main() {
 
     val igor = 3
     val pavel = 4
     val sasha = 3
     val petya = 5
-    val averageScore: Float = (3+4+3+5)/4f
+    val averageScore: Float = (igor + pavel + sasha + petya) / 4f
 
     println(averageScore)
 
