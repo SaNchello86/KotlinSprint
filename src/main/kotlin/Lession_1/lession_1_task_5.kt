80d6ba6b7bf6660d78d7626e6b3a9385d6da4bc1
SaNchello <sahokkk@mail.ru>
2023-04-10 21:12:17 +0300

    KS-1-5 10.04.2023 Вывод времени полёта минуты:секунды

--- src/main/kotlin/Lession_1/lession_1_task_5.kt
+++ src/main/kotlin/Lession_1/lession_1_task_5.kt
@@ -1,9 +1,13 @@
 package Lession_1
 
 fun main() {
-    val secmondsInSpace = 6480 // время полёта
-    val inSpaceMinute = 6480 / 60 - 60
+    val secmondsInSpace = 6480 // время полёта в секундах
+    val hous = 60 // минут
+    val minute = 60 // секунд
+    val oneSeconds = 1000 // миллисекунд
+    val inSpaceMinute = secmondsInSpace / minute - hous
+    val inSpaceSeconds = inSpaceMinute / oneSeconds
 
-    println ("$inSpaceMinute:00")
+    println(String.format("%02d:%02d", inSpaceMinute, inSpaceSeconds,))
 
 }
