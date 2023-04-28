041d58d7480ec156deb02104f96cc5f87cda5629
SaNchello <sahokkk@mail.ru>
2023-04-29 00:44:52 +0300

    KS-2-4 28.04.2023 Решение задачи №4

---
+++ src/main/kotlin/lession_2_task_4.kt
@@ -0,0 +1,11 @@
+class lession_2_task_4 {
+}fun main () {
+    val сrystalOre = 7
+    val ironOre = 11
+    val buff = 20
+    val сrystalOreBuff = сrystalOre % 20
+
+    println("Кристалическая руда - " + 7 % 20)
+    println("Железная руда - " + (ironOre))
+    println()
+}
