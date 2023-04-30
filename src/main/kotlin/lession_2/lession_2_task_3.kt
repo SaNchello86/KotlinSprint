01d8aa71f7e7b1b588f235d3ccef6ae0deefae2f
SaNchello <sahokkk@mail.ru>
2023-05-01 01:17:24 +0300

    KS-2-3 01.05.2023 Решение задачи №3, 2 темы.

--- src/main/kotlin/lession_2/lession_2_task_3.kt
+++ src/main/kotlin/lession_2/lession_2_task_3.kt
@@ -2,11 +2,11 @@ package lession_2
 
 fun main(){
     val travelTime: Int = 457 // время в пути в минутах
-    val hous: Int = 9 // часы выезда поезда
-    val minute: Int = 39 // минуты выезда поезда
-//    val oneSeconds: Int = 1000 // миллисекунд
-    val inSpaceMinute = travelTime / hous
-    val inSpaceSeconds = travelTime / 1000
+    val departureTimeHour: Int = 9 // часы выезда поезда
+    val departureTimeMinute: Int = 39 // минуты выезда поезда
+    val hour: Int = 60 // минут в 1 часе
+    val inSpaceMinute = travelTime + departureTimeMinute + departureTimeHour * hour
+    val inSpaceSeconds = inSpaceMinute % hour
 
-    println(String.format("%02d:%02d", inSpaceMinute, inSpaceSeconds,))
+    println(String.format("%02d:%02d", inSpaceMinute/hour, inSpaceSeconds,))
 }
