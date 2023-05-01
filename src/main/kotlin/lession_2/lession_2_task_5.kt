dea273533cc18a2f7a07fe4bf0f23e44c6c190d1
SaNchello <sahokkk@mail.ru>
2023-05-02 00:02:22 +0300

    KS-2-5 02.05.2023 Учтены замечания, задача №5 к 2-ой теме.

--- src/main/kotlin/lession_2/lession_2_task_5.kt
+++ src/main/kotlin/lession_2/lession_2_task_5.kt
@@ -3,10 +3,10 @@ package lession_2
 import kotlin.math.pow
 
 fun main () {
-    val depositAmount: Double = 70000.0
+    val depositAmount = 70000
     val interestRate: Double = 16.7
     val depositTerm: Double = 20.0
-    val amountOfIncome = depositAmount * ((1+interestRate/100).pow(depositTerm))
+    val amountOfIncome = depositAmount * ((1 + interestRate / 100).pow(depositTerm))
 
     println (String.format ("%.3f", amountOfIncome))
 
